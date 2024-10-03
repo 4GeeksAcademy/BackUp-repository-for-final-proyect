39c7e4954671a44d4454a585e33f2415a08bc1f8
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-10-03 19:49:43 +0000

    pages added

---
+++ src/front/js/pages/PRODUCERInfoForm.js
@@ -0,0 +1,103 @@
+
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { useParams, useNavigate } from "react-router-dom";
+
+
+export const ProducerInfoForm = () => {
+    const { store, actions } = useContext(Context);
+    const { producerId } = useParams()
+    const navigate = useNavigate();
+    const [ producerInfo, setProducerInfo ] = useState({});
+    const [ showSuccessMessage, setShowSuccessMessage ] = useState(false)
+
+    useEffect(()=>{
+        const currentProducer = store.producers.find(producer => producer.id === parseInt(producerId))
+        console.log("currentProducer in form", currentProducer)
+        if (currentProducer) {
+            setProducerInfo(currentProducer)
+        } else console.log("no info to return");
+        console.log("producerId", producerId);
+        console.log("producerInfo", producerInfo);
+        
+        
+     },[store.producers, producerId])//  
+    
+
+    const handleEdition = (e) => {
+        e.preventDefault();
+        actions.addProducerInfo(producerId, producerInfo)
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+            navigate(`/producer/dashboard/${localStorage.getItem("producerId")}`);
+        }, 2000);
+    }
+    const handleExit = () => {
+        navigate(`/producer/dashboard/${localStorage.getItem("producerId")}`)
+     }
+     const handleChange = (event) => {
+        const { name , value } = event.target
+        console.log("name from handleChange", name);
+        console.log("value from handleChange", value);
+        setProducerInfo((prevInfo) => ({...prevInfo, [name]: value
+     }));
+     }
+
+    return (
+        <>
+        <h1>Añade aquí tu información</h1>
+        {producerInfo ? (
+            <form onSubmit={handleEdition}>
+                <div className="mb-3">
+                    <label htmlFor="emailInput" className="form-label">Email</label>
+                    <input type="text" name="email" value={producerInfo.email || ''} onChange={handleChange} className="form-control" id="emailInput" disabled readOnly/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="brandNameInput" className="form-label">Nombre de la empresa</label>
+                    <input type="text" name="brand_name" value={producerInfo.brand_name || ''} onChange={handleChange} className="form-control" id="brandNameInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="userNameInput" className="form-label">Nombre de usuario</label>
+                    <input type="text" name="user_name" value={producerInfo.user_name || ''} onChange={handleChange} className="form-control" id="userNameInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="userLastNameInput" className="form-label">Apellido de usuario</label>
+                    <input type="text" name="user_last_name" value={producerInfo.user_last_name || ''} onChange={handleChange} className="form-control" id="userLastNameInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cifInput" className="form-label">CIF de la empresa</label>
+                    <input type="text" name="cif" value={producerInfo.cif || ''} onChange={handleChange} className="form-control" id="cifInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="addressInput" className="form-label">Dirección de la empresa</label>
+                    <input type="text" name="address" value={producerInfo.address || ''} onChange={handleChange} className="form-control" id="addressInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="provinceInput" className="form-label">Ciudad/Provincia</label>
+                    <input type="text" name="province" value={producerInfo.province || ''} onChange={handleChange} className="form-control" id="provinceInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="zipCodeInput" className="form-label">Código postal</label>
+                    <input type="text" name="zip_code" value={producerInfo.zip_code || ''} onChange={handleChange} className="form-control" id="zipCodeInput"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="phoneInput" className="form-label">Teléfono</label>
+                    <input type="number" name="phone" value={producerInfo.phone || ''} onChange={handleChange} className="form-control" id="phoneInput"/>
+                </div>
+                <div className="col-auto mb-3">
+                    <button type="submit" className="btn btn-primary me-3">Añadir</button>
+                    <button type="button" onClick={handleExit} className="btn btn-secondary">Salir</button>
+                </div>
+            </form>
+        ) : (
+        <p>Ups! Parece que ha habido un problema...</p>
+        )}
+        {showSuccessMessage &&
+        <div className="alert alert-success">Información añadida con éxito! 
+            <span className="spinner-border spinner-border-sm ms-3" aria-hidden="true"></span>
+            <span className="visually-hidden" role="status">Llevándote en tractor a Home!...</span>
+        </div>
+        }
+        </>
+    );
+};
---
+++ src/front/js/pages/producers.js
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import { ProducerLogin } from "../component/producerLogin";
+import { ProducerSignup } from "../component/producerSignup";
+
+export const Producers = () => {
+    const { store, actions } = useContext(Context);
+    const [ showSignup, setShowSignup ] = useState(false)
+
+    useEffect(()=>{
+        actions.getProducers();
+     },[])
+
+
+    return (
+        <>
+            <div>
+                
+                {showSignup ? (
+                    <ProducerSignup onBackToLogin={() => setShowSignup(false)} />
+                ) : (
+                    <ProducerLogin onSignup={() => setShowSignup(true)} />
+                )}
+                
+                {/* {showSignup ? (
+                    <ProducerSignup onBackToLogin={() => setShowSignup(false)} />
+                ) : (
+                    <>
+                        <h2>Producer Login</h2>
+                        <div className="mb-3">
+                            <label htmlFor="loginEmailInput" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="loginEmailInput" placeholder="name@example.com" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="loginPasswordInput" className="form-label">Password</label>
+                            <input type="password" id="loginPasswordInput" className="form-control" aria-describedby="passwordHelpBlock" />
+                        </div>
+                        <div className="mb-3">
+                            <span className="" onClick={() => setShowSignup(true)} style={{ cursor: 'pointer', color: 'blue' }}>
+                                or Sign Up here
+                            </span>
+                        </div>
+                        <button type="submit" className="signup btn btn-primary">Login</button>
+                    </>
+                )} */}
+            </div>
+        
+            {/* <h2>Producer List</h2>
+            <div className="container d-inline-flex my-4 gap-3">
+            {store.producers.length > 0 ? (
+                store.producers.map((producer, index) => 
+                    <div key={index}>
+                        <h3>{producer.brand_name || "no brand_name"}</h3>
+                        <p>{producer.email}</p>
+                        <p>{producer.user_name || "no username"} {producer.user_last_name || "no user_last_name"}</p>
+                        <p>{producer.address || "no address"}</p>
+                        <p>{producer.phone || "no phone"}</p>
+                        <button type="button" className="delete btn btn-danger" onClick={()=>actions.deleteProducer(producer.id)}>Delete</button>
+                        <Link to={"/producer/edit/" + producer.id}>
+                            <button type="button" className="edit btn btn-warning">Edit</button>
+						</Link>
+                        
+                    </div>
+                )
+            ) : (
+                <p>No producers found</p>
+            )}
+            </div> */}
+        </>
+    );
+};
