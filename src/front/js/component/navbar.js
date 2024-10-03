fc566693e430950818cdb60c63d4c7c9abc15938
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-10-03 19:42:10 +0000

    all components added

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -9,10 +10,29 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
+					<Link to="/producer/login">
+						<button className="btn btn-primary">Producers</button>
+					</Link>
+				</div>
+				<div className="ml-auto">
+					<Link to="/categories">
+						<button className="btn btn-primary">Categories</button>
+					</Link>
+				</div>
+				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
 					</Link>
+					<Link to="/product" className="mx-2">
+						<button className="btn btn-primary">View Products</button>
+					</Link>
 				</div>
+				<div>
+				<Link to="/customer/Login" className="mx-2">
+						<button className="btn btn-primary">SignUp</button>
+					</Link>
+				</div>
+				
 			</div>
 		</nav>
 	);
